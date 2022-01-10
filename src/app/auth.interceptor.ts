67ff82d2c9019b4028479574adb08616c65a3f9a
Mrinal Virnave <mrinalvirnave@users.noreply.github.com>
2022-01-09 20:16:13 -0800

    fixed login screen

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,10 +1,23 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Router } from "@angular/router";
+import { catchError, Observable, of, throwError } from "rxjs";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+    constructor(private router: Router) { }
 
+    private handleAuthError(err: HttpErrorResponse): Observable<any> {
+        //handle your auth error or rethrow
+        if (err.status === 401 || err.status === 403) {
+            //navigate /delete cookies or whatever
+            this.router.navigate(['login']);
+            // if you've caught / handled the error, you don't want to rethrow it unless you also want downstream consumers to have to handle it as well.
+            return of(err.message); // or EMPTY may be appropriate here
+        }
+        return throwError((err));
+    }
+    
     intercept(req: HttpRequest<any>,
               next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -16,7 +29,7 @@ export class AuthInterceptor implements HttpInterceptor {
                     "Bearer " + idToken)
             });
 
-            return next.handle(cloned);
+            return next.handle(cloned).pipe(catchError(x=> this.handleAuthError(x)));
         }
         else {
             return next.handle(req);
